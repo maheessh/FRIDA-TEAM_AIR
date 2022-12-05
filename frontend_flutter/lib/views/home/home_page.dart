686c44b5eecf47f5a973e5ba3a97e4aad782e3de
maheesssh <maheesssh@gmai.com>
2022-12-05 08:45:07 +0545

    dashboard debug

--- frontend_flutter/lib/views/home/home_page.dart
+++ frontend_flutter/lib/views/home/home_page.dart
@@ -84,183 +84,181 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w800),
         ),
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            width: size.width,
-            decoration: const BoxDecoration(color: Colors.white),
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  //Dashboard
-                  //this components shows the recommendation for the user
-                  //according to their skin condition
+      body: SingleChildScrollView(
+        child: Container(
+          width: size.width,
+          decoration: const BoxDecoration(color: Colors.white),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Dashboard
+                //this components shows the recommendation for the user
+                //according to their skin condition
 
-                  Padding(
-                    padding: EdgeInsets.only(top: size.height * 0.05),
-                    child: SizedBox(
-                      height: size.height * 0.1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Welcome,",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline5!
-                                .copyWith(color: Colors.black),
-                          ),
-                          Text(
-                            "DashBoard",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline4!
-                                .copyWith(
-                                    fontWeight: FontWeight.w300,
-                                    color: Colors.black),
-                          )
-                        ],
-                      ),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * 0.05),
+                  child: SizedBox(
+                    height: size.height * 0.1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Welcome,",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: Colors.black),
+                        ),
+                        Text(
+                          "DashBoard",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline4!
+                              .copyWith(
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.black),
+                        )
+                      ],
                     ),
                   ),
+                ),
 
-                  Padding(
-                    padding: EdgeInsets.only(top: size.height * 0.04),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        image: const DecorationImage(
-                            image: AssetImage(imageName), fit: BoxFit.fill),
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.white,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Color.fromARGB(57, 0, 0, 0),
-                            blurRadius: 5.0,
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * 0.04),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                          image: AssetImage(imageName), fit: BoxFit.fill),
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color.fromARGB(57, 0, 0, 0),
+                          blurRadius: 5.0,
+                        ),
+                      ],
+                    ),
+                    height: size.height * 0.3,
+                    width: size.width * 0.9,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Hi! Ashim Fatis.",
+                          style: TextStyle(
+                              fontSize: size.height * 0.035,
+                              color: const Color.fromARGB(255, 67, 108, 49),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Today is Sunny Day",
+                          style: TextStyle(
+                              fontSize: size.height * 0.02,
+                              color: const Color.fromARGB(255, 67, 108, 49),
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: size.height * 0.02),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Icon(
+                                Icons.thermostat,
+                                color: Colors.deepOrange,
+                              ),
+                              Text(
+                                "25 °C, 180mm of hg",
+                                style: TextStyle(
+                                    color: const Color.fromARGB(
+                                        255, 67, 108, 49),
+                                    fontSize: size.height * 0.03,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      height: size.height * 0.3,
-                      width: size.width * 0.9,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Hi! Bishal Fatis.",
-                            style: TextStyle(
-                                fontSize: size.height * 0.035,
-                                color: const Color.fromARGB(255, 67, 108, 49),
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            "Today is Sunny Day",
-                            style: TextStyle(
-                                fontSize: size.height * 0.02,
-                                color: const Color.fromARGB(255, 67, 108, 49),
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(top: size.height * 0.02),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.thermostat,
-                                  color: Colors.deepOrange,
-                                ),
-                                Text(
-                                  "25 °C, 180mm of hg",
-                                  style: TextStyle(
-                                      color: const Color.fromARGB(
-                                          255, 67, 108, 49),
-                                      fontSize: size.height * 0.03,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
+                ),
 
-                  //! Today Component
-                  //! Personalized message
-                  const PersonalizedComponent(),
+                //! Today Component
+                //! Personalized message
+                const PersonalizedComponent(),
 
-                  /* 
-                    events,
-                    news,
-                    funds component
-                  */
+                /*
+                  events,
+                  news,
+                  funds component
+                */
 
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: size.height * 0.01),
-                    child: ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: lists.length,
-                        itemBuilder: (context, index) {
-                          Map<String, dynamic> list = lists[index];
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: size.height * 0.01),
+                  child: ListView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> list = lists[index];
 
-                          return Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: size.height * 0.02),
-                            child: Material(
+                        return Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: size.height * 0.02),
+                          child: Material(
+                            borderRadius: BorderRadius.circular(12.0),
+                            child: InkWell(
                               borderRadius: BorderRadius.circular(12.0),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(12.0),
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => list["route"]));
-                                },
-                                child: Ink(
-                                  height: size.height * 0.15,
-                                  decoration: BoxDecoration(
-                                      gradient: LinearGradient(colors: [
-                                        list["colors"][0],
-                                        list["colors"][1]
-                                      ]),
-                                      borderRadius:
-                                          BorderRadius.circular(12.0)),
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(
-                                        list["iconData"],
-                                        size: size.height * 0.05,
-                                        color: Colors.white,
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => list["route"]));
+                              },
+                              child: Ink(
+                                height: size.height * 0.15,
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [
+                                      list["colors"][0],
+                                      list["colors"][1]
+                                    ]),
+                                    borderRadius:
+                                        BorderRadius.circular(12.0)),
+                                child: Row(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Icon(
+                                      list["iconData"],
+                                      size: size.height * 0.05,
+                                      color: Colors.white,
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(
+                                          left: size.width * 0.06),
+                                      child: Text(
+                                        list["name"],
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline5!
+                                            .copyWith(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),
                                       ),
-                                      Padding(
-                                        padding: EdgeInsets.only(
-                                            left: size.width * 0.06),
-                                        child: Text(
-                                          list["name"],
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold),
-                                        ),
-                                      )
-                                    ],
-                                  ),
+                                    )
+                                  ],
                                 ),
                               ),
                             ),
-                          );
-                        }),
-                  )
-                ],
-              ),
+                          ),
+                        );
+                      }),
+                )
+              ],
             ),
           ),
         ),
